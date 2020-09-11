b7af06298630ebaf3c0908ff28ea1056e9d55c4f
Wercton <werctonmatheus@gmail.com>
2020-09-11 20:17:16 -0300

    minor changes

--- Plattformspiel/configuracoes.py
+++ Plattformspiel/configuracoes.py
@@ -10,9 +10,6 @@ RECORDE_FILE = '.recorde.txt'
 # cores
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 BG_COR_SAVE = [0, 155, 155]
 
